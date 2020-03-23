7ac0df414844c1b87f047e3c58e251c57ad6956f
KittyKat <kathrynlle9@gmail.com>
2020-03-23 18:52:34 -0400

    "added image and fixed font"

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -8,10 +8,12 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
@@ -19,26 +21,31 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.Group;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+
+import java.awt.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
         primaryStage.setTitle("Assignments Page");
         GridPane grid = new GridPane();
         grid.setVgap(10.0);
         grid.setPadding(new Insets(25, 25, 25, 25));
 
-        FlowPane pane = new FlowPane(10, 10);
-        pane.setBorder(new Border(new BorderStroke(Color.BLACK,
-                BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
-
-        Scene scene = new Scene(grid, 350, 500);
+        Scene scene = new Scene(grid, 500, 550);
         primaryStage.setScene(scene);
 
         Text scenetitle = new Text("Assignments");
         scenetitle.setFont(Font.font("Times New Roman", FontWeight.NORMAL, 20));
         grid.add(scenetitle, 0, 0, 1, 1);
+
 
         int amountOfAssignments = 5;
         for (int assignmentNumber = 1; assignmentNumber <= amountOfAssignments; assignmentNumber++) {
@@ -46,10 +53,16 @@ public class Main extends Application {
             int rowIndex = assignmentNumber * 2;
 
             Label assignment = new Label("Assignment #" + assignmentNumber + ":");
+            assignment.setFont((Font.font("Cambria", 25)));
             grid.add(assignment, columnIndex, rowIndex);
 
-            Label dueDate = new Label("Due:");
-            grid.add(dueDate, columnIndex, rowIndex + 1);
+            //set date
+            DateFormat dueDate = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG);
+            Calendar cal = Calendar.getInstance();
+
+            Label due = new Label("Due: " + dueDate.format(cal.getTime()));
+            grid.add(due, columnIndex, rowIndex + 1);
+
 
             //ADD BUTTON TO INDICATE ASSIGNMENT IS OPENED
             Button assignmentButton = new Button();
@@ -57,6 +70,15 @@ public class Main extends Application {
             //Try to add a border
 
             //Try to add an image
+            Image assignmentImage = new Image("https://www.learnupon.com/wp-content/uploads/assignments720.png", 100,100, true,false);
+            grid.add(new ImageView(assignmentImage), 0, rowIndex);
+
+
+
+
+
+
+
 
 
 
